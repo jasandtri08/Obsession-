964f8e70bf6bb228dc3e1d87ab4014f618af9cc5
jasandtri08 <amuwachisom@gmail.com>
2026-08-18 13:08:49 +0100

    Add files via upload

---
+++ Index2.js
@@ -0,0 +1,82 @@
+const game = document.getElementById("game");
+
+game.innerHTML = `
+  <div class="sparkles">✦ ✧ ✦</div>
+
+  <img src="Images22/Ribbons and cherries.png" class="ribbons">
+  <img src="Images22/Skulls and love.png" class="skulls">
+  <img src="Images22/Skulls left.png" class="skull">
+  <img src="Images22/Pictureoflace.png" class="lace">
+  <img src="Images22/Deadrose.png" class="rose">
+
+  <img src="Images22/Deadrose.png" class="dead">
+
+  <h1>MANIA</h1>
+
+  <button id="startButton">𓆩♡𓆪 ENTER 𓆩♡𓆪</button>
+
+  <div class="quote">
+    You loved her once. You will love her again.
+  </div>
+`;
+
+const startButton = document.getElementById("startButton");
+startButton.addEventListener("click", function () {
+
+  startButton.style.display = "none";
+
+  const opening = document.createElement("div");
+  opening.id = "opening";
+
+  opening.innerHTML = `
+    <div class="dots">• • •</div>
+  `;
+
+  game.appendChild(opening);
+
+  setTimeout(() => {
+
+    opening.remove();
+
+    showStory();
+
+  }, 10000);
+
+});
+function showStory() {
+  
+
+  game.innerHTML = `
+    <div id="story">
+
+      <img src="Images22/Intricate.png" class="intricate top-left">
+      <img src="Images22/Intricate.png" class="intricate top-right">
+      <img src="Images22/Intricate.png" class="intricate bottom-left">
+      <img src="Images22/Intricate.png" class="intricate bottom-right">
+
+      <div class="story-text">
+
+        <p id="line1">She was beautiful</p>
+
+        <p id="line2">You loved her</p>
+
+        <p id="line3">You killed her</p>
+
+      </div>
+
+    </div>
+  `;
+
+  setTimeout(() => {
+    document.getElementById("line1").classList.add("show");
+  }, 500);
+
+  setTimeout(() => {
+    document.getElementById("line2").classList.add("show");
+  }, 3000);
+
+  setTimeout(() => {
+    document.getElementById("line3").classList.add("show");
+  }, 5500);
+
+}
